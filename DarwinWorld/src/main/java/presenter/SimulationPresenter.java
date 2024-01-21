c4736c0a75cd161824c11288941b1347fbafe83e
Monika Etrych <monika.etrych@gmail.com>
2024-01-21 23:44:13 +0100

    energy label

--- DarwinWorld/src/main/java/presenter/SimulationPresenter.java
+++ DarwinWorld/src/main/java/presenter/SimulationPresenter.java
@@ -1,25 +1,21 @@
 package presenter;
 
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.HPos;
-import javafx.scene.control.Button;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.*;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
 import model.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class SimulationPresenter implements MapChangeListener {
     private final int CELL_WIDTH = 40;
     private final int CELL_HEIGHT = 40;
     @FXML
     public GridPane gridMap;
-    @FXML
-    public Button startButton;
     @FXML
     private WorldMap map;
 
@@ -47,11 +43,25 @@ public class SimulationPresenter implements MapChangeListener {
         });
     }
 
-    private void addLabel(String text, int colIndex, int rowIndex) {
+    private void addLabel(String text, int columnIndex, int rowIndex) {
         Label label = new Label(text);
         GridPane.setHalignment(label, HPos.CENTER);
-        gridMap.add(label, colIndex, rowIndex);
+        gridMap.add(label, columnIndex, rowIndex);
     }
+
+    private void addElementLabel(String text1, String text2, int columnIndex, int rowIndex){
+        Label label1 = new Label(text1);
+        Label label2 = new Label(text2);
+        GridPane.setHalignment(label1, HPos.CENTER);
+        GridPane.setHalignment(label2, HPos.CENTER);
+
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
+        vbox.getChildren().addAll(label1, label2);
+
+        gridMap.add(vbox, columnIndex, rowIndex);
+    }
+
 
     public void drawMap(WorldMap map){
         clearGrid();
@@ -60,6 +70,7 @@ public class SimulationPresenter implements MapChangeListener {
         int height = size.getY();
 
         Boundary boundary = map.getCurrentBounds();
+
         addLabel("x/y", 0, 0);
         gridMap.getColumnConstraints().add(new ColumnConstraints(CELL_WIDTH));
         gridMap.getRowConstraints().add(new RowConstraints(CELL_HEIGHT));
@@ -79,9 +90,14 @@ public class SimulationPresenter implements MapChangeListener {
             for (int y = 0; y <= height; y++) {
                 Vector2d position = new Vector2d(x + boundary.lowerLeft().getX(), boundary.upperRight().getY() - y);
                 WorldElement element = map.objectAt(position);
-                String EMPTY_CELL = " ";
-                String labelContent = (element == null) ? EMPTY_CELL : element.toString();
-                addLabel(labelContent, x + 1, y + 1);
+                String labelContent = " ";
+                String energy = " ";
+                if(element != null) {
+                    labelContent = element.toString();
+                    if(element instanceof Animal)
+                        energy = Integer.toString (((Animal) element).getEnergy());
+                }
+                addElementLabel(labelContent, energy, x+1, y+1);
             }
         }
     }
